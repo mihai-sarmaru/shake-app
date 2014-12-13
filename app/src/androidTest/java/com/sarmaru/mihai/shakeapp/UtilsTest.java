17c6893496ab57e7ec10220d9953fe72015acce7
Mihai Sarmaru <predator7w@gmail.com>
2014-12-13 12:35:02 +0200

    Add UtilsTest class to verify that data and time are formatted correctly

---
+++ app/src/androidTest/java/com/sarmaru/mihai/shakeapp/UtilsTest.java
@@ -0,0 +1,23 @@
+package com.sarmaru.mihai.shakeapp;
+
+import android.test.InstrumentationTestCase;
+
+/**
+ * Created by Mihai Sarmaru on 13.12.2014.
+ */
+public class UtilsTest extends InstrumentationTestCase {
+
+    private long millis = 1418454278;
+
+    public void testFormatDate () {
+        String formattedDate = Utils.formatDate(millis * 1000);
+        String expectedDate = "13.12.2014";
+        assertEquals(formattedDate, expectedDate);
+    }
+
+    public void testFormatTime () {
+        String formattedTime = Utils.formatTime(millis * 1000);
+        String expectedTime = "09:04:38";
+        assertEquals(formattedTime, expectedTime);
+    }
+}
