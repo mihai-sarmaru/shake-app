df5d16a14b4323e89be9f49a188f70b0cf9b211b
Mihai Sarmaru <predator7w@gmail.com>
2015-02-10 18:52:21 +0200

    Add and setup RecyclerView to MainActivity

--- app/src/main/java/com/sarmaru/mihai/shakeapp/MainActivity.java
+++ app/src/main/java/com/sarmaru/mihai/shakeapp/MainActivity.java
@@ -2,16 +2,23 @@ package com.sarmaru.mihai.shakeapp;
 
 import android.content.IntentFilter;
 import android.net.ConnectivityManager;
+import android.os.AsyncTask;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
 
     SwipeRefreshLayout swipeRefreshLayout;
+    RecyclerView quakeRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +31,13 @@ public class MainActivity extends ActionBarActivity {
         // Register receiver to check for Internet connection
         registerReceiver(new ConnectionChangeReceiver(),
                 new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
-    }
 
+        quakeRecyclerView = (RecyclerView) findViewById(R.id.quake_main_list);
+        quakeRecyclerView.setHasFixedSize(true);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        quakeRecyclerView.setLayoutManager(layoutManager);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
