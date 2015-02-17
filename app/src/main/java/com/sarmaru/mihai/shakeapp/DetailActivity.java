11e003836f23647e0677cff2d6b932f493557a0e
Mihai Sarmaru <predator7w@gmail.com>
2015-02-17 19:30:36 +0200

    Add initializeMap method for starting up Google Maps

--- app/src/main/java/com/sarmaru/mihai/shakeapp/DetailActivity.java
+++ app/src/main/java/com/sarmaru/mihai/shakeapp/DetailActivity.java
@@ -3,12 +3,18 @@ package com.sarmaru.mihai.shakeapp;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+
 
 public class DetailActivity extends ActionBarActivity {
+
+    private GoogleMap googleMap;
 
     private static final String QUAKEID = "QUAKEID";
     private QuakeObject quake = null;
@@ -26,8 +32,14 @@ public class DetailActivity extends ActionBarActivity {
             Toast.makeText(this, getString(R.string.toast_no_quake_info), Toast.LENGTH_LONG).show();
             finish();
         }
-    }
 
+        try {
+            initializeMap();
+        } catch (Exception ex) {
+            Log.d("MAPS", "Failed initializing maps");
+            ex.printStackTrace();
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -44,5 +56,21 @@ public class DetailActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        initializeMap();
+    }
+
+    private void initializeMap () {
+        if (googleMap == null) {
+            googleMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+            if (googleMap == null) {
+                Toast.makeText(getApplicationContext(),
+                        "Unable to create map", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
