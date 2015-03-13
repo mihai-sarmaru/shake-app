4a75fb3321b4fcbcbc11e81a5edc3a8fd2300d11
Mihai Sarmaru <predator7w@gmail.com>
2015-03-13 12:57:55 +0200

    Add launchNotification private method to launch a notification

--- app/src/main/java/com/sarmaru/mihai/shakeapp/QuakeNotifications.java
+++ app/src/main/java/com/sarmaru/mihai/shakeapp/QuakeNotifications.java
@@ -1,6 +1,11 @@
 package com.sarmaru.mihai.shakeapp;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 
 /**
  * Created by Mihai Sarmaru on 13.03.2015.
@@ -13,5 +18,16 @@ public class QuakeNotifications {
         this.context = mContext;
     }
 
-    
+    private void launchNotification(Intent intent, String title, String content) {
+        NotificationCompat.Builder notification = new NotificationCompat.Builder(context)
+                .setSmallIcon(R.drawable.ic_launcher)
+                .setContentTitle(title)
+                .setContentText(content);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        notification.setContentIntent(pendingIntent);
+
+        NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.notify(100, notification.build());
+    }
 }
