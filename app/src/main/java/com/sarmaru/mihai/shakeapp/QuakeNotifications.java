f4755f7b60916c097890231ba30879c14375febf
Mihai Sarmaru <predator7w@gmail.com>
2015-03-13 11:57:07 +0200

    Add QuakeNotifications class with constructor and private context

---
+++ app/src/main/java/com/sarmaru/mihai/shakeapp/QuakeNotifications.java
@@ -0,0 +1,17 @@
+package com.sarmaru.mihai.shakeapp;
+
+import android.content.Context;
+
+/**
+ * Created by Mihai Sarmaru on 13.03.2015.
+ */
+public class QuakeNotifications {
+
+    private Context context;
+
+    public QuakeNotifications (Context mContext) {
+        this.context = mContext;
+    }
+
+    
+}
